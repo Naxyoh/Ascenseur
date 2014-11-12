bfebf5608e200a15fe25b2285608d97f68eb7b76
Naxyoh <yoan.smit@gmail.com>
2014-11-12 10:36:35 +0100

    Create Deck.h

---
+++ Deck.h
@@ -0,0 +1,28 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<ctime>
+#include<cstdlib>
+#include"Carte.h"
+
+class Deck
+{
+    public:
+        Deck();
+        Deck(bool); // Sert à initialiser un deck vierge
+        virtual ~Deck();
+        void ajouterCartes(std::vector<Carte>); //ajoute une liste de carte à un deck
+        void ajouterCarte(Carte*); // ajoute une seule carte à un deck
+        //Carte piocherCarte(int, int);
+        void piocherCarte(int, Deck&); // tirer un nombre de carte
+        void afficherDeck() const; //affiche le contenu d'un deck
+        std::vector<Carte> getDeck();
+    protected:
+        std::vector<Carte> m_listeCarte;
+    private:
+};
+
+#endif // DECK_H

822eaf0dc8ce85801221cc3ba486ee3125dd05f1
Naxyoh <yoan.smit@gmail.com>
2014-11-12 10:37:15 +0100

    Create Carte.h

---
+++ Carte.h
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+enum Couleur{carreau, coeur, pique, trefle};
+enum Valeur{deux=2, trois, quatre, cinq, six, sept, huit, neuf, dix, J, Q, K, A};
+
+class Carte
+{
+	public :
+
+		Carte();
+		Carte(Couleur c, Valeur v);
+		~Carte();
+		void afficher() const;
+
+		Couleur getCouleur() const;
+		Valeur getValeur() const;
+		void setCouleur(Couleur c);
+		void setValeur(Valeur v);
+	protected :
+	    Couleur m_couleur;
+		Valeur m_valeur;
+	private :
+
+
+};
+
+bool operator==(Carte const& a, Carte const& b);
+bool operator!=(Carte const& a, Carte const& b);
+bool operator<(Carte const &a, Carte const& b);
+

287b7c084cb329b69b302a00ed6e4f2f21b043bb
Naxyoh <yoan.smit@gmail.com>
2014-11-16 22:01:07 +0100

    Update Joueur.cpp

--- Joueur.cpp
+++ Joueur.cpp
@@ -1,6 +1,13 @@
 #include "Joueur.h"
 
-Joueur::Joueur() : m_nom(""), m_annonce(0), m_dealer(false), m_main(Deck()), m_pliEffectue(0), m_score(0)
+using namespace std;
+
+Joueur::Joueur() : m_nom(""), m_annonce(0), m_pliEffectue(0), m_score(0), m_dealer(false), m_main(Deck(false))
+{
+    //ctor
+}
+
+Joueur::Joueur(string nom) : m_nom(nom), m_annonce(0), m_pliEffectue(0), m_score(0), m_dealer(false), m_main(Deck(false))
 {
     //ctor
 }
@@ -60,7 +67,34 @@ Deck Joueur::getMain()
 {
     return m_main;
 }
-void Joueur::setMain(Deck main)
+void Joueur::setMain(Deck& main)
 {
     m_main = main;
+}
+
+std::vector<Carte> Joueur::getCarteJouee()
+{
+    return m_carteJouee;
+}
+
+void Joueur::jouerCarte(int numCarte)
+{
+    vector<Carte> maMain = m_main.getDeck();
+    Carte carteJouee = maMain[numCarte-1];
+    m_main.retirerCarte(numCarte);
+    m_carteJouee.push_back(carteJouee);
+
+}
+
+void Joueur::afficherJoueur()
+{
+    cout<<"Joueur : "<<m_nom<<endl;
+    cout<<"Annonce : "<<m_annonce<<endl;
+    cout<<"Pli effectue dans cette manche : "<<m_pliEffectue<<endl;
+    cout<<"Dealer : "<<m_dealer<<endl;
+    cout<<"Score : "<<m_score<<endl;
+    cout<<"Main : "<<endl;
+    m_main.afficherDeck();
+    cout<<endl;
+
 }
